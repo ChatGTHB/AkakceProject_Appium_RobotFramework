deec4f7bc131737318ba5d8975ff0dfe7f5a5dd7
ChatGTHB <cihat.kose@hotmail.com>
2023-07-13 20:35:07 +0300

    StringGenerator.py added

---
+++ Libraries/StringGenerator.py
@@ -0,0 +1,22 @@
+from faker import Faker
+fake = Faker()
+
+
+def generate_name():
+    name = fake.name()
+    return name.split(" ")[0]
+
+
+def generate_surname():
+    surname = fake.name()
+    return surname.split(" ")[1]
+
+
+def generate_email():
+    email = fake.email()
+    return email
+
+
+def generate_password():
+    password = fake.password()
+    return password
